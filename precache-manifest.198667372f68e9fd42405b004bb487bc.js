074be97e4e7ab0cfeab7edc5efe281cbc58079a8
Prosperity <prosperauthor@gmail.com>
2020-08-21 00:49:13 +0100

    Updates

---
+++ precache-manifest.198667372f68e9fd42405b004bb487bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f817c3b06e6ad3cea5b4",
+    "url": "/Blockchain-social-network/static/css/main.68b705fc.chunk.css"
+  },
+  {
+    "revision": "f817c3b06e6ad3cea5b4",
+    "url": "/Blockchain-social-network/static/js/main.f817c3b0.chunk.js"
+  },
+  {
+    "revision": "8b9a95d7a4200da14cba",
+    "url": "/Blockchain-social-network/static/css/1.711e82d5.chunk.css"
+  },
+  {
+    "revision": "8b9a95d7a4200da14cba",
+    "url": "/Blockchain-social-network/static/js/1.8b9a95d7.chunk.js"
+  },
+  {
+    "revision": "d0b66679790e9eede80f",
+    "url": "/Blockchain-social-network/static/js/runtime~main.d0b66679.js"
+  },
+  {
+    "revision": "cc56f11576dc6b6b390bc76602cdf505",
+    "url": "/Blockchain-social-network/index.html"
+  }
+];
